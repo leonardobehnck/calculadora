4a9563b6f8c9bb0c1d352bf0e5ae90093fad4c4d
Leonardo Behnck <leonardo.behnck@gmail.com>
2022-04-02 12:33:52 -0300

    Correções

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Calculadora
+
+![](https://img.shields.io/github/license/leonardobehnck/advinhacao)
+![](https://img.shields.io/github/stars/leonardobehnck/advinhacao)
+![](https://img.shields.io/github/forks/leonardobehnck/advinhacao)
+![](https://img.shields.io/github/issues/leonardobehnck/advinhacao)
+
+## 📁 Acesso ao projeto
+
+<a href="https://refined-github-html-preview.kidonng.workers.dev/leonardobehnck/calculadora/raw/main/calculadora.html">Clique aqui para acessar a demo do projeto.</a>
+
+## :hammer: Funcionalidades do projeto
+
+- `Funcionalidade 1`:
+- `Funcionalidade 2`:
+- `Funcionalidade 3`:
+- `Funcionalidade 4`:
+
+![Badge em Desenvolvimento](http://img.shields.io/static/v1?label=STATUS&message=EM%20DESENVOLVIMENTO&color=GREEN&style=for-the-badge)
--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 let updateMode = false
 
 function calc(event) {
-    let op = event.srcElement.innerHTML
+  let op = event.srcElement.innerHTML
 
-        if (updateMode) {
-            updateMode = false
-                if (parseInt(op) > 0) {
-                    document.getElementById('userInp').value = op
-                    return
-                }
-        }
+  if (updateMode) {
+    updateMode = false
+    if (parseInt(op) > 0) {
+      document.getElementById('userInp').value = op
+      return
+    }
+  }
 
-        if (op == "C") {
-            document.getElementById('userInp').value = ""
-            return
-        }
+  if (op == "C") {
+    document.getElementById('userInp').value = ""
+    return
+  }
 
-        if (op != "=") {
-            document.getElementById('userInp').value += op
-        }   else {
-            document.getElementById('userInp').value = eval(document.getElementById('userInp').value)
-            updateMode = true
-        }
+  if (op != "=") {
+    document.getElementById('userInp').value += op
+  } else {
+    document.getElementById('userInp').value = eval(document.getElementById('userInp').value)
+    updateMode = true
+  }
 }
